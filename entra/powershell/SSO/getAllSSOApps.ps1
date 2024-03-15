61992b40cbc7b8401a96845d7bcea16985ea4b58
Esam Farraj_a <efarraj_a@saumdev.com>
2024-03-15 17:21:06 -0500

    3-15-prep

---
+++ entra/powershell/SSO/getAllSSOApps.ps1
@@ -0,0 +1,9 @@
+## Script to pull all applciations that are congifured for SSO
+## to review the configuraitons, status, certificate, client
+## credentials, API permissions, etc
+
+#### Requirements ####
+## Get Access Token
+## Obtain list of Applications in Entra ID
+## Filter for applications with SSO configured (SAML/OIDC)
+## Export to CSV
